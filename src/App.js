1a9d50411fcfc406b3192f91445870130a9e8641
ShivaMisraa <shivam18557@gmail.com>
2023-03-24 22:55:16 +0530

    This is updated code(previous has some typos)

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
   const expenses=[
     {id:"e1",
     title:"Car Insuarance", 
-    ammount:294.67,
+    amount:294.67,
     date: new Date(2021,2,28),
     location:"Insuarance Ltd."},
 
     {id:"e2",
     title:"Toilet Paper",
-    ammount:450.25, 
+    amount:450.25, 
     date: new Date(2021,12,18),
     location:"City Kart"},
 
     {id:"e3",
     title:"Car Reapairing",
-    ammount:685.57,
+    amount:685.57,
     date: new Date(2021,8,8),
   location:"Reapairing Center"},
 
     {id:"e4",
     title:"Buying Goods",
-    ammount:805.01,
+    amount:805.01,
     date: new Date(2021,4,12),
   location:"City kart"},
   ];
@@ -33,25 +33,25 @@ function App() {
       
       <ExpenseItem
        title={expenses[0].title} 
-       ammount={expenses[0].ammount} 
+       amount={expenses[0].amount} 
        date={expenses[0].date} 
        location={expenses[0].location}
        />
       <ExpenseItem 
       title={expenses[1].title}
-       ammount={expenses[1].ammount}
+       amount={expenses[1].amount}
         date={expenses[1].date} 
         location={expenses[1].location}
         /> 
       <ExpenseItem 
       title={expenses[2].title}
-       ammount={expenses[2].ammount} 
+       amount={expenses[2].amount} 
        date={expenses[2].date}
        location={expenses[2].location} 
        /> 
       <ExpenseItem 
       title={expenses[3].title}
-       ammount={expenses[3].ammount} 
+       amount={expenses[3].amount} 
        date={expenses[3].date} 
        location={expenses[3].location}
        />
--- src/Components/ExpenseDetails.js
+++ src/Components/ExpenseDetails.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css';
 
 function ExpenseDetails(props){
-    const ammount= props.ammount;
+    
+    const amount= props.amount;
     const location= props.location;
 
     return(
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
-            <div className='expense-item__price'>Rs {ammount}</div>
+            <div className='expense-item__price'> Rs{amount}</div>
             <div className='expense-item__price'> {location}</div>
             </div>
     )
