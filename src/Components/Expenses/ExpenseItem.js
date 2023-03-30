b1293fb9fc1e8bad1626818663986674bef1c9a4
ShivaMisraa <shivam18557@gmail.com>
2023-03-30 21:41:53 +0530

    Added buttons to change exoense amount and title.

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const clickHandler= () =>{
-    console.log("clicked")
-  };
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
 
+  const clickHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
+  const clickToAmount = () => {
+    setAmount("100");
+    console.log(amount);
+  };
 
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">Rs{props.amount}</div>
-        
+        <h2>{title}</h2>
+        <div className="expense-item__price">Rs{amount}</div>
+
+        <button onClick={clickToAmount}>Change amount</button>
         <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
